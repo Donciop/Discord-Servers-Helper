ee04801769c92509d34ae0f1b8b2d2c97bf690e8
Donciop <donciop123@gmail.com>
2022-01-13 19:05:23 +0100

    improved comments

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ client.load_extension("tftCommands")
 async def on_ready():
     """ Event handler that is called when bot is turned on. """
     print("Bot is ready")
-    await client.change_presence(
+    await client.change_presence(   # change the bot's description on Discord member list
       activity=discord.Activity(
-        type=discord.ActivityType.watching,
-        name="small kids *help"
+        type=discord.ActivityType.watching,  # get the "is watching ..." format
+        name="small servers *help"
       )
     )
 
@@ -47,10 +47,10 @@ async def on_ready():
 @client.event
 async def on_raw_reaction_add(payload):
     """ Event handler for handling reactions to messages. Here's used to assign roles based on reaction. """
-    guild = await client.fetch_guild(payload.guild_id)
+    guild = await client.fetch_guild(payload.guild_id)  # get information about member and member's server
     member = await guild.fetch_member(payload.user_id)
-    if payload.channel_id == 748120165001986128 and payload.message_id == 884555735671918642:
-        if str(payload.emoji) == "❌":
+    if payload.channel_id == 748120165001986128 and payload.message_id == 884555735671918642:   # check if we react to desired message
+        if str(payload.emoji) == "❌":   # switch-case for every emote / every role.
             role = guild.get_role(877347765519253544)
         if str(payload.emoji) == "1️⃣":
             role = guild.get_role(757291879195738322)
@@ -61,7 +61,7 @@ async def on_raw_reaction_add(payload):
         if str(payload.emoji) == "🎸":
             role = guild.get_role(883802534580457522)
         if role is not None:
-            await payload.member.add_roles(role)
+            await payload.member.add_roles(role)  # add role to member
             print(f"Added {role} to {member}")
 
 
@@ -82,7 +82,7 @@ async def on_raw_reaction_remove(payload):
         if str(payload.emoji) == "🎸":
             role = guild.get_role(883802534580457522)
         if role is not None:
-            await member.remove_roles(role)
+            await member.remove_roles(role)  # remove role from member
             print(f"Removed {role} from {member}")
 
 # Tasks
@@ -94,13 +94,14 @@ async def reminder():
     current_time = datetime.datetime.now()  # get current time
     hour = current_time.hour
     minute = current_time.minute
-    bot_channel = client.get_channel(796794980810620948)
+
+    bot_channel = client.get_channel(796794980810620948)  # check if we're sending message in right channel
     if hour == 12 and minute == 00:  # check the time
-        await bot_channel.send("Wybiło południe")
+        await bot_channel.send("Wybiło południe")  # send actual message
 
 
 @reminder.before_loop
-async def before():
+async def before():  # wait for bot to go online to start the task
     await client.wait_until_ready()
 
 # Error handling
@@ -110,13 +111,13 @@ async def before():
 async def on_command_error(ctx, error):
     """ Error handling for exceptions when using commands. Called when bot encounters an error. """
     if isinstance(error, commands.CommandOnCooldown):  # called when you try to use command that is on cooldown.
-        embed = discord.Embed(color=0xeb1414)
+        embed = discord.Embed(color=0xeb1414)  # Discord embed formatting
         embed.add_field(
           name="🛑 Command Error",
           value="Command's on cooldown. Time remaining: {}s :(".format(round(error.retry_after)),
           inline=False
         )
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed)  # send the embed
         return
     if isinstance(error, commands.MissingPermissions):  # called when you don't have permission to use that command.
         embed = discord.Embed(color=0xeb1414)
