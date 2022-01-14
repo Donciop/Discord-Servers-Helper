e0a71de801662262406fbad60a9fe0ee874305c9
Donciop <donciop123@gmail.com>
2022-01-14 20:01:25 +0100

    fixed manageUsersCommands.py and manageMessagesCommands.py

--- manageMessagesCommands.py
+++ manageMessagesCommands.py
@@ -1,47 +1,85 @@
 import discord
-import typing
 from discord.ext import commands
+from discord_slash import cog_ext  # for slash commands
+from discord_slash.model import SlashCommandPermissionType
+from discord_slash.utils.manage_commands import create_option, create_permission
 from main import client
-class manageMessagesCommands(commands.Cog):
-  def __init__(self, client):
-    self.client = client
-   
-  @commands.command()
-  @commands.has_permissions(manage_messages = True)
-  @commands.cooldown(1, 10, commands.BucketType.user)
-  async def msgCount(self, ctx, channel: typing.Optional[discord.TextChannel]):
-    embed=discord.Embed(color=0x11f80d)
-    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-    if not channel:
-      channel = ctx.channel
-    count = 0
-    async with ctx.typing():
-      async for msg in channel.history(limit=None):
-        count += 1
-      embed.add_field(name="✅ Message Count Successfull", value=f"There were {count} messages in {channel.mention}", inline=False)
-      await ctx.send(embed=embed)
-      
-  @commands.command()
-  @commands.has_permissions(manage_messages = True)
-  @commands.cooldown(1, 1, commands.BucketType.user)
-  async def clear(self, ctx, amount: int = 0):
-    embed=discord.Embed(color=0xeb1414)
-    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-    if not amount:
-      embed.add_field(name="🛑 Clear Error", value="Specify amount of messages to clear", inline=False)
-      await ctx.send(embed=embed)
-    elif amount > 20:
-      embed.add_field(name="🛑 Clear Error", value="Too many messages to clear! (max: 20)", inline=False)
-      await ctx.send(embed=embed)
-    else:
-      await ctx.channel.purge(limit=amount+1)
 
-  @clear.error
-  async def clear_error(self, ctx, error):
-    if isinstance(error, commands.BadArgument):
-      embed=discord.Embed(color=0xeb1414)
-      embed.add_field(name="🛑 Clear Error", value="Wrong number of messages", inline=False)
-      await ctx.send(embed=embed)
 
-def setup(client):
-  client.add_cog(manageMessagesCommands(client))
+class ManageMessagesCommands(commands.Cog):
+    def __init__(self, client):
+      self.client = client
+
+    @cog_ext.cog_slash(  # slash command decorator
+        name="msg_count_channel",  # name that will be displayed in Discord
+        description="Count messages in specific discord's channel",  # description of the command
+        permissions={
+            218510314835148802: [
+                create_permission(545658751689031699, SlashCommandPermissionType.ROLE, True),
+                create_permission(748144455730593792, SlashCommandPermissionType.ROLE, True),
+                create_permission(826094492309258291, SlashCommandPermissionType.ROLE, True),
+                create_permission(541960938165764096, SlashCommandPermissionType.ROLE, False),
+                create_permission(541961631954108435, SlashCommandPermissionType.ROLE, False)
+            ]
+        },
+        guild_ids=[218510314835148802],  # list of server (guilds) id's that have access to this slash command
+        options=[
+            create_option(  # parameters in slash command
+              name="channel",  # name of the variable
+              description="Select channel",  # description of the parameter
+              option_type=7,  # option_type refers to type of the variable ( 7 - CHANNEL )
+              required=True  # this parameter is required
+            )
+        ]
+    )
+    async def msg_count_channel(self, ctx, channel):
+        """
+        Command used to count all messages in specific channel
+
+        :param ctx: passing context of the command
+        :param channel: discord's channel
+        """
+        if not ctx.author.guild_permissions.manage_messages:  # check if author of the command has right permissions
+            await ctx.send("You don't have permission to unban members")
+            return  # return if author's missing permission
+        await ctx.defer()
+        count = 0
+        async for msg in channel.history(limit=None):  # iterate over every message in channel's history
+            count += 1  # increment count for every message
+        await ctx.send(f"There were {count} messages in {channel.mention}")  # display amount of messages in channel
+
+    @commands.command()  # decorator for discord.py commands
+    @commands.has_permissions(manage_messages=True)  # check if user has permission to use that command
+    @commands.cooldown(1, 1, commands.BucketType.user)  # assign cooldown for this command
+    async def clear(self, ctx, amount: int = 0):
+        """
+        Command used to delete specific amount of messages from discord's channel
+
+        :param ctx: passing context of the command
+        :param amount: amount of messages to delete
+        """
+        if not amount:  # check if user specified number of messages
+            await ctx.send("Specify amount of messages to clear")
+            return  # return if amount of messages wasn't specified
+        elif amount > 20 or amount <= 0:  # check if number of messages is correct
+            await ctx.send("Wrong amount of messages! (min: 1, max: 20)")
+            return  # return if wrong number of messages was specified
+        else:
+            await ctx.channel.purge(limit=amount+1)  # delete that amount of messages along with message with this command
+
+    @clear.error  # decorator for handling errors from 'clear' command
+    async def clear_error(self, ctx, error):
+        """
+        Error handling for 'clear' command
+
+        :param ctx: passing context of the command
+        :param error: discord.py error that occured when executing command
+        """
+        if isinstance(error, commands.BadArgument):  # check if error's type is 'Bad Argument'
+            embed = discord.Embed(color=0xeb1414)  # style embed message
+            embed.add_field(name="🛑 Clear Error", value="Wrong number of messages", inline=False)
+            await ctx.send(embed=embed)  # send specific return message
+
+
+def setup(client):  # adding cog to our main.py file
+    client.add_cog(ManageMessagesCommands(client))
--- manageUsersCommands.py
+++ manageUsersCommands.py
@@ -9,20 +9,20 @@ class ManageUsersCommands(commands.Cog):
 
     @cog_ext.cog_slash(  # slash command decorator
         name="unban",  # name that will be displayed in Discord
-        description="Check a specific player's Teamfight Tactics rank",  # description of the command
+        description="Unban specific member from this discord server",  # description of the command
         guild_ids=[218510314835148802],  # list of server (guilds) id's that have access to this slash command
         options=[
             create_option(  # parameters in slash command
-              name="nickname",  # name of the variable
-              description="Type in player's nickname",  # description of the parameter
-              option_type=3,  # option_type refers to type of the variable ( 3 - STRING )
-              required=True  # this parameter is required
+                name="nickname",  # name of the variable
+                description="Type in player's nickname",  # description of the parameter
+                option_type=3,  # option_type refers to type of the variable ( 3 - STRING )
+                required=True  # this parameter is required
             ),
             create_option(
-              name="discriminator",
-              description="Type in player's discriminator",
-              option_type=4,  # (4 - INTEGER)
-              required=True
+                name="discriminator",
+                description="Type in player's discriminator",
+                option_type=3,
+                required=True
             )
         ]
     )
@@ -34,15 +34,20 @@ class ManageUsersCommands(commands.Cog):
         :param nickname: Discord's nickname
         :param discriminator: Discord's discriminator (for ex. #1234)
         """
-        if not ctx.author.guild_permissions.manage_messages:  # check if author of the command has right permissions
+        if not ctx.author.guild_permissions.ban_members:  # check if author of the command has right permissions
             await ctx.send("You don't have permission to unban members")
             return  # return if author's missing permission
         banned_users = await ctx.guild.bans()  # access banned users of that discord server
+        not_found = 0
         for ban_entry in banned_users:  # iterate over every ban entry in discord logs
             user = ban_entry.user
             if (user.name, user.discriminator) == (nickname, discriminator):  # check if banned user matches our member
+                not_found = 1
                 await ctx.guild.unban(user)  # un-ban if the member matches the banned user
                 await ctx.send(f'**{user}** has been unbanned!')
+        if not_found == 0:  # check if user is user is banned
+            await ctx.send(f"Seems like {nickname} #{discriminator} isn't banned on this server")
+            return  # return if user's not banned on server
 
 
 def setup(client):  # adding cog to our main.py file
