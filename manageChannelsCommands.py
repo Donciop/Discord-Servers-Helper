16b0610d86192e5e7130a61840161cdc090ef2a8
Donciop <donciop123@gmail.com>
2022-01-15 04:20:54 +0100

    fixed administratorCommands.py and comments

--- manageChannelsCommands.py
+++ manageChannelsCommands.py
@@ -1,121 +1,147 @@
 import discord
+from discord.ext import commands
+from discord_slash import cog_ext  # for slash commands
+from discord_slash.model import SlashCommandPermissionType
+from discord_slash.utils.manage_commands import create_option, create_permission
+from main import client
 import typing
 import asyncio
 import random
-from discord.ext import commands
-from main import client
 
-class manageChannelsCommands(commands.Cog):
-  def __init__(self, client):
-    self.client = client
 
-  @commands.Cog.listener()
-  async def on_ready(self):
-    self.newsChannel = client.get_channel(748120165001986128)
-    self.botChannel = client.get_channel(888056072538042428)
-    self.testChannel = client.get_channel(902710519646015498)
-    self.owner = client.get_user(198436287848382464)
-    self.guild = client.get_guild(218510314835148802)
-  
-  @commands.command()
-  @commands.has_permissions(mute_members = True)
-  @commands.cooldown(1, 10, commands.BucketType.user)
-  async def mute(self, ctx, member: typing.Optional[discord.Member], mute_minutes: typing.Optional[int] = 0,*, reason: typing.Optional[str] = ""):
-    embed=discord.Embed(color=0xeb1414)
-    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-    if member:
-      if ctx.channel.id == self.botChannel or ctx.channel.id == self.testChannel or ctx.channel.id == self.newsChannel:
-        await member.edit(mute = True)
-        embed=discord.Embed(color=0x11f80d)
-        if not mute_minutes:
-          embed.add_field(name="🔇 Mute Successful", value=f"**{member.display_name}** was muted by **{ctx.author.display_name}**", inline=False)
+class ManageChannelsCommands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.newsChannel = client.get_channel(748120165001986128)
+        self.botChannel = client.get_channel(888056072538042428)
+        self.testChannel = client.get_channel(902710519646015498)
+        self.owner = client.get_user(198436287848382464)
+        self.guild = client.get_guild(218510314835148802)
+
+    @cog_ext.cog_slash(  # slash command decorator
+        name="mute",  # name that will be displayed in Discord
+        description="Mute someone",  # description of the command
+        guild_ids=[218510314835148802],  # list of server (guilds) id's that have access to this slash command
+        options=[
+            create_option(  # parameters in slash command
+                name="member",  # name of the variable
+                description="Choose who to mute",  # description of the parameter
+                option_type=3,  # option_type refers to type of the variable ( 3 - STRING )
+                required=True  # this parameter is required
+            ),
+            create_option(
+                name="mute_minutes",
+                description="How long to mute",
+                option_type=4,
+                required=True
+            ),
+            create_option(
+                name="reason",
+                description="Reason why to mute someone",
+                option_type=3,
+                required=False
+            )
+        ]
+    )
+    async def mute(self, ctx, member, mute_minutes, reason):
+        embed = discord.Embed(color=0xeb1414)
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        if member:
+            if ctx.channel.id == self.botChannel or ctx.channel.id == self.testChannel or ctx.channel.id == self.newsChannel:
+                await member.edit(mute = True)
+                embed=discord.Embed(color=0x11f80d)
+                if not mute_minutes:
+                    embed.add_field(name="🔇 Mute Successful", value=f"**{member.display_name}** was muted by **{ctx.author.display_name}**", inline=False)
+                else:
+                    embed.add_field(name="🔇 Mute Successful", value=f"**{member.display_name}** was muted by **{ctx.author.display_name}** for 🕐 **{str(mute_minutes)}** minutes", inline=False)
+                if reason:
+                    embed.add_field(name="📜 Reason", value=reason, inline=False)
+            else:
+                embed.add_field(name="🛑 Mute Failed", value=f"You can't post commands outside of {self.botChannel.mention} dumbass", inline=False)
         else:
-          embed.add_field(name="🔇 Mute Successful", value=f"**{member.display_name}** was muted by **{ctx.author.display_name}** for 🕐 **{str(mute_minutes)}** minutes", inline=False)
-        if reason:
-          embed.add_field(name="📜 Reason", value=reason, inline=False)
-      else:
-        embed.add_field(name="🛑 Mute Failed", value=f"You can't post commands outside of {self.botChannel.mention} dumbass", inline=False)
-    else:
-      embed.add_field(name="🛑 Mute Failed", value="You have to specify a Discord member to mute",inline=False)
-    await ctx.send(embed=embed)
-    if mute_minutes > 0:
-      await asyncio.sleep(mute_minutes * 60)
-      await member.edit(mute = False)
-    
-  @commands.command()
-  @commands.has_permissions(mute_members = True)
-  @commands.cooldown(1, 10, commands.BucketType.user)
-  async def deaf(self, ctx, member: typing.Optional[discord.Member], deaf_minutes: typing.Optional[int] = 0,*, reason: typing.Optional[str] = ""):
-    embed=discord.Embed(color=0xeb1414)
-    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-    if member:
-      if ctx.channel.id == self.botChannel.id or ctx.channel.id == self.newsChannel.id or ctx.channel.id == self.testChannel.id:
-        await member.edit(deafen = True)
-        embed=discord.Embed(color=0x11f80d)
-        if not deaf_minutes:
-          embed.add_field(name="🔇 Deaf Successful", value=f"**{member.display_name}** was deafened by **{ctx.author.display_name}**", inline=False)
-        else:
-          embed.add_field(name="🔇 Deaf Successful", value=f"**{member.display_name}** was deafened by **{ctx.author.display_name}** for 🕐 **{str(deaf_minutes)}** minutes", inline=False)
-        if reason:
-          embed.add_field(name="📜 Reason", value=reason, inline=False)
-      else:
-        embed.add_field(name="🛑 Deaf Failed", value=f"You can't post commands outside of {self.botChannel.mention} dumbass", inline=False)
-    else:
-      embed.add_field(name="🛑 Deaf Failed", value="You have to specify a Discord member to mute",inline=False)
-    await ctx.send(embed=embed)
-    if deaf_minutes > 0:
-      await asyncio.sleep(deaf_minutes * 60)
-      await member.edit(deaf = False)
+            embed.add_field(name="🛑 Mute Failed", value="You have to specify a Discord member to mute",inline=False)
+        await ctx.send(embed=embed)
+        if mute_minutes > 0:
+          await asyncio.sleep(mute_minutes * 60)
+          await member.edit(mute = False)
 
-  @commands.command()
-  @commands.has_permissions(mute_members = True)
-  @commands.cooldown(1, 300, commands.BucketType.user)
-  async def mutecf(self, ctx, member: typing.Optional[discord.Member]):
-    mute_minutes_self = 0
-    mute_minutes = 0
-    number = random.randint(0,100)
-    embed=discord.Embed(color=0xeb1414)
-    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-    if member:
-      if ctx.channel.id == self.botChannel.id or ctx.channel.id == self.newsChannel.id or ctx.channel.id == self.testChannel.id:
-        if ctx.message.author != member:
-          if number >= 50:
-            embed=discord.Embed(color=0x11f80d)
-            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-            await member.edit(mute = True)
-            embed.add_field(name="✅ Mute Coinflip Succesful", value=f"You've rolled {str(number)} and muted {member.display_name} for 1 minute", inline=False)
-            mute_minutes += 1
+    @commands.command()
+    @commands.has_permissions(mute_members = True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def deaf(self, ctx, member: typing.Optional[discord.Member], deaf_minutes: typing.Optional[int] = 0,*, reason: typing.Optional[str] = ""):
+      embed=discord.Embed(color=0xeb1414)
+      embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+      if member:
+        if ctx.channel.id == self.botChannel.id or ctx.channel.id == self.newsChannel.id or ctx.channel.id == self.testChannel.id:
+          await member.edit(deafen = True)
+          embed=discord.Embed(color=0x11f80d)
+          if not deaf_minutes:
+            embed.add_field(name="🔇 Deaf Successful", value=f"**{member.display_name}** was deafened by **{ctx.author.display_name}**", inline=False)
           else:
-            await ctx.author.edit(mute = True)
-            embed.add_field(name="🛑 Mute Coinflip Failed", value=f"You've rolled {str(number)} and failed to mute {member.display_name}, however you got muted for 3 minutes", inline=False)
-            mute_minutes_self += 3
+            embed.add_field(name="🔇 Deaf Successful", value=f"**{member.display_name}** was deafened by **{ctx.author.display_name}** for 🕐 **{str(deaf_minutes)}** minutes", inline=False)
+          if reason:
+            embed.add_field(name="📜 Reason", value=reason, inline=False)
         else:
-          embed.add_field(name="🛑 Mute Coinflip Failed", value="You can't coinflip with yourself dumbass", inline=False)
+          embed.add_field(name="🛑 Deaf Failed", value=f"You can't post commands outside of {self.botChannel.mention} dumbass", inline=False)
       else:
-        embed.add_field(name="🛑 Mute Coinflip Failed", value=f"You can't post commands outside of {self.botChannel.mention} dumbass", inline=False)
-    else:
-      embed.add_field(name="🛑 Mute Coinflip Failed", value="You have to specify a Discord member to mute",inline=False)
-    await ctx.send(embed=embed)
-    if mute_minutes_self > 0:
-      await asyncio.sleep(mute_minutes_self * 60)
-      await ctx.author.edit(mute = False)
-    if mute_minutes > 0:
-      await asyncio.sleep(mute_minutes * 60)
-      await member.edit(mute = False)
-
-  @mute.error
-  async def mute_error(self, ctx, error):
-    if isinstance(error, commands.BadArgument):
-      embed=discord.Embed(color=0xeb1414)
-      embed.add_field(name="🛑 Mute Error", value="Enter proper time (in minutes)", inline=False)
+        embed.add_field(name="🛑 Deaf Failed", value="You have to specify a Discord member to mute",inline=False)
       await ctx.send(embed=embed)
+      if deaf_minutes > 0:
+        await asyncio.sleep(deaf_minutes * 60)
+        await member.edit(deaf = False)
 
-  @deaf.error
-  async def deaf_error(self, ctx, error):
-    if isinstance(error, commands.BadArgument):
+    @commands.command()
+    @commands.has_permissions(mute_members = True)
+    @commands.cooldown(1, 300, commands.BucketType.user)
+    async def mutecf(self, ctx, member: typing.Optional[discord.Member]):
+      mute_minutes_self = 0
+      mute_minutes = 0
+      number = random.randint(0,100)
       embed=discord.Embed(color=0xeb1414)
-      embed.add_field(name="🛑 Deaf Error", value="Enter proper time (in minutes)", inline=False)
+      embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+      if member:
+        if ctx.channel.id == self.botChannel.id or ctx.channel.id == self.newsChannel.id or ctx.channel.id == self.testChannel.id:
+          if ctx.message.author != member:
+            if number >= 50:
+              embed=discord.Embed(color=0x11f80d)
+              embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+              await member.edit(mute = True)
+              embed.add_field(name="✅ Mute Coinflip Succesful", value=f"You've rolled {str(number)} and muted {member.display_name} for 1 minute", inline=False)
+              mute_minutes += 1
+            else:
+              await ctx.author.edit(mute = True)
+              embed.add_field(name="🛑 Mute Coinflip Failed", value=f"You've rolled {str(number)} and failed to mute {member.display_name}, however you got muted for 3 minutes", inline=False)
+              mute_minutes_self += 3
+          else:
+            embed.add_field(name="🛑 Mute Coinflip Failed", value="You can't coinflip with yourself dumbass", inline=False)
+        else:
+          embed.add_field(name="🛑 Mute Coinflip Failed", value=f"You can't post commands outside of {self.botChannel.mention} dumbass", inline=False)
+      else:
+        embed.add_field(name="🛑 Mute Coinflip Failed", value="You have to specify a Discord member to mute",inline=False)
       await ctx.send(embed=embed)
+      if mute_minutes_self > 0:
+        await asyncio.sleep(mute_minutes_self * 60)
+        await ctx.author.edit(mute = False)
+      if mute_minutes > 0:
+        await asyncio.sleep(mute_minutes * 60)
+        await member.edit(mute = False)
+
+    @mute.error
+    async def mute_error(self, ctx, error):
+      if isinstance(error, commands.BadArgument):
+        embed=discord.Embed(color=0xeb1414)
+        embed.add_field(name="🛑 Mute Error", value="Enter proper time (in minutes)", inline=False)
+        await ctx.send(embed=embed)
+
+    @deaf.error
+    async def deaf_error(self, ctx, error):
+      if isinstance(error, commands.BadArgument):
+        embed=discord.Embed(color=0xeb1414)
+        embed.add_field(name="🛑 Deaf Error", value="Enter proper time (in minutes)", inline=False)
+        await ctx.send(embed=embed)
 
 def setup(client):
-  client.add_cog(manageChannelsCommands(client))
+  client.add_cog(ManageChannelsCommands(client))
