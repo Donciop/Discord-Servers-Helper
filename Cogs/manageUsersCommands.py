7b4d533f306a03528e4607ed699403045f7e4e9f
Dzony <donciop123@gmail.com>
2022-04-05 01:03:31 +0200

    fixed permissions and docstring

--- Cogs/manageUsersCommands.py
+++ Cogs/manageUsersCommands.py
@@ -1,7 +1,6 @@
 from discord.ext import commands  # main packages
 from discord_slash import cog_ext  # for slash commands
-from discord_slash.model import SlashCommandPermissionType
-from discord_slash.utils.manage_commands import create_option, create_permission
+from discord_slash.utils.manage_commands import create_option
 from Cogs.settingsCommands import SettingsCommands
 
 
@@ -12,16 +11,6 @@ class ManageUsersCommands(commands.Cog):
     @cog_ext.cog_slash(  # slash command decorator
         name="dc_unban",  # name that will be displayed in Discord
         description="Unban specific member from this discord server",  # description of the command
-        guild_ids=[218510314835148802],  # list of server (guilds) id's that have access to this slash command
-        permissions={
-            218510314835148802: [
-                create_permission(545658751689031699, SlashCommandPermissionType.ROLE, True),
-                create_permission(748144455730593792, SlashCommandPermissionType.ROLE, False),
-                create_permission(826094492309258291, SlashCommandPermissionType.ROLE, False),
-                create_permission(541960938165764096, SlashCommandPermissionType.ROLE, False),
-                create_permission(541961631954108435, SlashCommandPermissionType.ROLE, False)
-            ]
-        },
         options=[
             create_option(  # parameters in slash command
                 name="nickname",  # name of the variable
@@ -37,7 +26,8 @@ class ManageUsersCommands(commands.Cog):
             )
         ]
     )
-    async def unban(self, ctx, nickname, discriminator):
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, nickname: str, discriminator: str):
         """
         Command used to un-ban members that are banned on server (guild)
 
@@ -45,6 +35,9 @@ class ManageUsersCommands(commands.Cog):
                 ctx: Context of the command
                 nickname (str): Nickname of the person we want to unban
                 discriminator (str): Discord discriminator that is used after every nickname
+
+            Returns:
+                None
         """
         channel_check = await SettingsCommands.channel_check(ctx)
         if not channel_check:
