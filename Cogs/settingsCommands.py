10f155aa84a4cb1b649c2908c015dbf5753f6498
Dzony <donciop123@gmail.com>
2022-06-04 16:06:35 +0200

    refactored whole file, moved commands to different classes, fixed most of them, removed old, redundant code.

--- Cogs/settingsCommands.py
+++ Cogs/settingsCommands.py
@@ -2,39 +2,171 @@ import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands, application_checks
 from nextcord.abc import GuildChannel
-import requests.exceptions
-from typing import Union
 from pymongo import MongoClient
-from os import getenv, makedirs, path
-from json import loads
-import logging
 from riotwatcher import TftWatcher, LolWatcher
+import requests.exceptions
+import requests
+import typing
+import os
+import json
 
-logging.basicConfig(level=logging.INFO)
 
-
-class SettingsCommands(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+class LolUtilityFunctions(commands.Cog):
 
     @staticmethod
-    async def load_json_dict(filepath: str):
+    async def get_summoner(interaction: nextcord.Interaction, nickname: str):
         """
-        Utility method that is used to load .json files as Python dictionaries
+        Utility method for accessing player's account data from Riot API
 
             Args:
-                filepath (str): File path to desired .json file
+                interaction (nextcord.Interaction): Context of the command
+                nickname (str): Nickname of the plyer
 
             Returns:
-                final_dict (dict): Dictionary contains passed .json file
+                summoner (dict): Dictionary containing information about specific player
         """
-        with open(filepath) as file:
-            temp_dict = file.read()
-            final_dict = loads(temp_dict)
-        return final_dict
+        watcher = LolWatcher(os.getenv('APIKEYLOL'))
+        try:
+            summoner = watcher.summoner.by_name('eun1', nickname)
+            return summoner
+        except requests.exceptions.HTTPError:
+            await interaction.response.send_message(f'Can\'t find **{nickname}** on EUNE server', ephemeral=True)
+            return None
 
     @staticmethod
-    async def db_connection(db: str, collection: str, *, interaction: nextcord.Interaction = None):
+    async def get_soloq_ranked_stats(interaction: nextcord.Interaction, summoner):
+        """
+        Utility method used to gather information about player's rank
+
+            Args:
+                interaction (nextcord.Interaction): Context of the command
+                from Teamfight Tactics (TFT)
+                summoner (summonerDTO): Represents summoner as Python dictionary
+
+            Returns:
+                summoner_stats (dict): Information about player's rank
+        """
+        watcher = LolWatcher(os.getenv('APIKEYLOL'))
+
+        summoner_stats = watcher.league.by_summoner('eun1', summoner['id'])
+        if not summoner_stats:
+            await interaction.response.send_message(f'Player {summoner["name"]} is unranked', ephemeral=True)
+            return None
+
+        queue_found = False
+        for league_type in summoner_stats:
+            if league_type['queueType'] == 'RANKED_SOLO_5x5':
+                return league_type
+
+        if not queue_found:
+            await interaction.response.send_message(f'Player {summoner["name"]} is unranked', ephemeral=True)
+            return None
+
+    @staticmethod
+    async def get_flex_ranked_stats(interaction: nextcord.Interaction, summoner):
+        """
+        Utility method used to gather information about player's rank
+
+            Args:
+                interaction (nextcord.Interaction): Context of the command
+                from Teamfight Tactics (TFT)
+                summoner (summonerDTO): Represents summoner as Python dictionary
+
+            Returns:
+                summoner_stats (dict): Information about player's rank
+        """
+        watcher = LolWatcher(os.getenv('APIKEYLOL'))
+
+        summoner_stats = watcher.league.by_summoner('eun1', summoner['id'])
+        if not summoner_stats:
+            await interaction.response.send_message(f'Player {summoner["name"]} is unranked', ephemeral=True)
+            return None
+
+        queue_found = False
+        for league_type in summoner_stats:
+            if league_type['queueType'] == 'RANKED_TEAM_5x5':
+                return league_type
+
+        if not queue_found:
+            await interaction.response.send_message(f'Player {summoner["name"]} is unranked', ephemeral=True)
+            return None
+
+
+class TftUtilityFunctions(commands.Cog):
+
+    @staticmethod
+    async def get_summoner(interaction: nextcord.Interaction, nickname: str):
+        """
+        Utility method for accessing player's account data from Riot API
+
+            Args:
+                interaction (nextcord.Interaction): Context of the command
+                nickname (str): Nickname of the plyer
+
+            Returns:
+                summoner (dict): Dictionary containing information about specific player
+        """
+        watcher = TftWatcher(os.getenv('APIKEYTFT'))
+        try:
+            summoner = watcher.summoner.by_name('eun1', nickname)
+            return summoner
+        except requests.exceptions.HTTPError:
+            await interaction.response.send_message(f'Can\'t find **{nickname}** on EUNE server', ephemeral=True)
+            return None
+
+    @staticmethod
+    async def get_tft_ranked_stats(interaction: nextcord.Interaction, summoner):
+        watcher = TftWatcher(os.getenv('APIKEYTFT'))
+
+        summoner_stats = watcher.league.by_summoner('eun1', summoner['id'])
+        if not summoner_stats:
+            await interaction.response.send_message(f'Player {summoner} is unranked', ephemeral=True)
+            return None
+
+        queue_found = False
+        for league_type in summoner_stats:
+            if league_type['queueType'] == 'RANKED_TFT':
+                return league_type
+
+        if not queue_found:
+            await interaction.response.send_message(f'Player {summoner} is unranked', ephemeral=True)
+            return
+
+
+class RiotUtilityFunctions(commands.Cog):
+
+    @staticmethod
+    async def get_rank_emoji(summoner_rank: dict):
+        rank_dict = await SettingsCommands.load_json_dict("JsonData/rankDict.json")
+        for tier in rank_dict:  # iterate over every tier until we find player's tier
+            if summoner_rank['tier'] == tier:
+                tier_emoji = rank_dict[tier]
+                return tier_emoji
+
+    @staticmethod
+    async def get_local_rank(summoner_rank: dict):
+        local_rank = 0
+        rank_list = ['I', 'II', 'III', 'IV']
+        rank_dict = await SettingsCommands.load_json_dict("JsonData/rankDict.json")
+
+        for tier in rank_dict:
+            if summoner_rank['tier'] != tier:
+                local_rank += 400
+            else:
+                break
+
+        for lol_rank in rank_list:  # iterate over every rank until we find player's rank
+            if summoner_rank['rank'] != lol_rank:
+                local_rank += 100
+            else:
+                local_rank += summoner_rank['leaguePoints']  # calculate final rating for leaderboard
+                return local_rank
+
+
+class DatabaseManager(commands.Cog):
+
+    @staticmethod
+    async def get_db_collection(db: str, collection: str, *, interaction: nextcord.Interaction = None):
         """
         Utility method that is used to connect to the MongoDB Database
 
@@ -46,7 +178,7 @@ class SettingsCommands(commands.Cog):
             Returns:
                 collection: Collection from the Database
         """
-        mongo_client = MongoClient(getenv('MONGOURL'))
+        mongo_client = MongoClient(os.getenv('MONGOURL'))
         db = mongo_client[db]
         collection = db[collection]
         if collection is None:
@@ -56,33 +188,29 @@ class SettingsCommands(commands.Cog):
         return collection
 
     @staticmethod
-    async def channel_check(interaction: Interaction):
+    async def upload_members_to_database(guild: nextcord.Guild):
         """
-        Utility method that is used to check if user is sending commands in channel that allows it
-
-            Args:
-                interaction (nextcord.Interaction): ???
+        Utility method used to import server members into database
 
             Returns:
-                bool: True if user can send messages in this channel, False otherwise
+                None
         """
-        channel_check = False
-        collection = await SettingsCommands.db_connection('Discord_Bot_Database', 'guild_bot_channels')
+
+        collection = await DatabaseManager.get_db_collection('Discord_Bot_Database', 'new_members')
         if collection is None:
             return
-        check = collection.find_one({'_id': interaction.guild_id})
-        if not check:
-            channel_check = True
-        else:
-            for bot_channel in check['bot_channels']:
-                if interaction.channel_id == int(bot_channel):
-                    channel_check = True
+        for member in guild.members:
+            if member.bot:
+                continue
+            check = collection.find_one({'_id': member.id})
+            if not check:
+                collection.insert_one({'_id': member.id, 'messages_sent': 0})
+            else:
+                collection.update_one({'_id': member.id},
+                                      {'$set': {'messages_sent': 0}})
 
-        # sends specific message if command is used in forbidden channel
-        if not channel_check:
-            await interaction.response.send_message(f'Please, use bot commands in bot channel to prevent spam',
-                                                    ephemeral=True)
-        return channel_check
+
+class FilesManager(commands.Cog):
 
     @staticmethod
     async def create_attachments_dir(*, filepath: str, channel: GuildChannel):
@@ -96,12 +224,12 @@ class SettingsCommands(commands.Cog):
             Returns:
                 None
         """
-        exists = path.exists(f'{filepath}\\{channel.name}')
+        exists = os.path.exists(f'{filepath}\\{channel.name}')
         if not exists:
-            makedirs(f'{filepath}\\{channel.name}\\Video')
-            makedirs(f'{filepath}\\{channel.name}\\Text')
-            makedirs(f'{filepath}\\{channel.name}\\Uncategorized')
-            makedirs(f'{filepath}\\{channel.name}\\Images')
+            os.makedirs(f'{filepath}\\{channel.name}\\Video')
+            os.makedirs(f'{filepath}\\{channel.name}\\Text')
+            os.makedirs(f'{filepath}\\{channel.name}\\Uncategorized')
+            os.makedirs(f'{filepath}\\{channel.name}\\Images')
             return
 
     @staticmethod
@@ -124,7 +252,7 @@ class SettingsCommands(commands.Cog):
                      'Video': ['mp4', 'mov', 'webm'],
                      'Text': ['pdf', 'txt']}
         created_time = msg.created_at.strftime("%Y_%m_%d_%H_%M_%S")
-        await SettingsCommands.create_attachments_dir(filepath=filepath, channel=channel)
+        await FilesManager.create_attachments_dir(filepath=filepath, channel=channel)
         filetype_found = False
         for i_counter, attachment in enumerate(msg.attachments):
             for category, category_filetypes in filetypes.items():
@@ -132,7 +260,7 @@ class SettingsCommands(commands.Cog):
                     if attachment.filename.lower().endswith(filetype):
                         fp = f'{filepath}\\{channel.name}\\{category}\\{counter}' \
                              f'_{i_counter}_{msg.author.name}_{created_time}.{filetype}'
-                        exists = path.exists(f'{filepath}\\{channel.name}')
+                        exists = os.path.exists(f'{filepath}\\{channel.name}')
                         if not exists:
                             return False
                         try:
@@ -149,118 +277,57 @@ class SettingsCommands(commands.Cog):
                 except nextcord.HTTPException:
                     return False
 
+
+class SettingsCommands(commands.Cog):
+
     @staticmethod
-    async def get_riot_stats(interaction: nextcord.Interaction, *, stats_type: str, nickname: str, all_stats=False):
+    async def load_json_dict(filepath: str):
         """
-        Utility method used to gather information about player's rank
+        Utility method that is used to load .json files as Python dictionaries
 
             Args:
-                interaction (nextcord.Interaction): Context of the command
-                stats_type (str): Whether we gather stats from League of Legends (LOL) or Teamfight Tactics (TFT)
-                nickname (str): Player's nickname
-                all_stats (bool, optional): if True, gather information about all queue types
-                from Teamfight Tactics (TFT)
+                filepath (str): File path to desired .json file
 
             Returns:
-                summoner (dict): Information about players profile
-                summoner_stats (dict): Information about player's rank
+                final_dict (dict): Dictionary contains passed .json file
         """
-        watcher = TftWatcher(getenv('APIKEYTFT'))
-        stats_name = 'RANKED_TFT'
-        if stats_type.lower() == 'lol':
-            watcher = LolWatcher(getenv('APIKEYLOL'))
-            stats_name = 'RANKED_SOLO_5x5'
-        try:
-            summoner = watcher.summoner.by_name('eun1', nickname)
-        except requests.exceptions.HTTPError:
-            summoner_stats = []
-            summoner = ''
-            await interaction.response.send_message(f'Can\'t find **{nickname}** on EUNE server', ephemeral=True)
-            return summoner, summoner_stats
-
-        summoner_stats = watcher.league.by_summoner('eun1', summoner['id'])
-        if not summoner_stats:
-            summoner_stats = []
-            await interaction.response.send_message(f'Player {nickname} is unranked', ephemeral=True)
-            return summoner, summoner_stats
-
-        if all_stats:
-            return summoner, summoner_stats
-        queue_found = False
-        for league_type in summoner_stats:
-            if league_type['queueType'] == stats_name:
-                return summoner, league_type
-            else:
-                queue_found = False
-        if not queue_found:
-            summoner_stats = []
-            await interaction.response.send_message(f'Player {nickname} is unranked', ephemeral=True)
-            return summoner, summoner_stats
+        with open(filepath) as file:
+            temp_dict = file.read()
+            final_dict = json.loads(temp_dict)
+        return final_dict
 
     @staticmethod
-    async def riot_rank_check(summoner_rank: dict, *, tier_emoji=True, rank=True):
+    async def channel_check(interaction: Interaction):
         """
-        Method that checks player's rank and return specific information for further usage
+        Utility method that is used to check if user is sending commands in channel that allows it
 
             Args:
-                summoner_rank (dict): Dictionary from RiotWatcher containing information about player's ranking
-                tier_emoji (bool): if True, method will return specific emoji associated with ranked tier
-                rank (bool): if True, method will return local rating
+                interaction (nextcord.Interaction): ???
 
             Returns:
-                tier_emoji: custom emoji based on player's ranking
-                rank (int): calculated rating for local leaderboard
+                bool: True if user can send messages in this channel, False otherwise
         """
-        rank_dict = await SettingsCommands.load_json_dict("JsonData/rankDict.json")
-        rank_list = ['I', 'II', 'III', 'IV']
-        local_rank = 0
-        local_tier_emoji = None
-        if not summoner_rank:
-            return local_tier_emoji, local_rank
-        for tier in rank_dict:  # iterate over every tier until we find player's tier
-            if summoner_rank['tier'] != tier:
-                local_rank += 400
-            else:
-                local_tier_emoji = rank_dict[tier]
-                break
-        for lol_rank in rank_list:  # iterate over every rank until we find player's rank
-            if summoner_rank['rank'] != lol_rank:
-                local_rank += 100
-            else:
-                break
-        local_rank += summoner_rank['leaguePoints']  # calculate final rating for leaderboard
-        if rank and tier_emoji:
-            return local_tier_emoji, local_rank
-        elif not tier_emoji:
-            return rank
-        else:
-            return local_tier_emoji
-
-    @staticmethod
-    async def upload_members_to_database(guild: nextcord.Guild):
-        """
-        Utility method used to import server members into database
-
-            Returns:
-                None
-        """
-
-        collection = await SettingsCommands.db_connection('Discord_Bot_Database', 'new_members')
+        channel_check = False
+        collection = await DatabaseManager.get_db_collection('Discord_Bot_Database', 'guild_bot_channels')
         if collection is None:
             return
-        for member in guild.members:
-            if member.bot:
-                continue
-            check = collection.find_one({'_id': member.id})
-            if not check:
-                collection.insert_one({'_id': member.id, 'messages_sent': 0})
-            else:
-                collection.update_one({'_id': member.id},
-                                      {'$set': {'messages_sent': 0}})
+        check = collection.find_one({'_id': interaction.guild_id})
+        if not check:
+            channel_check = True
+        else:
+            for bot_channel in check['bot_channels']:
+                if interaction.channel_id == int(bot_channel):
+                    channel_check = True
+
+        # sends specific message if command is used in forbidden channel
+        if not channel_check:
+            await interaction.response.send_message(f'Please, use bot commands in bot channel to prevent spam',
+                                                    ephemeral=True)
+        return channel_check
 
     @staticmethod
-    async def format_missing_permissions(error: Union[application_checks.ApplicationMissingPermissions,
-                                                      commands.MissingPermissions, commands.BotMissingPermissions]):
+    async def format_missing_permissions(error: typing.Union[application_checks.ApplicationMissingPermissions,
+                                                             commands.MissingPermissions, commands.BotMissingPermissions]):
         """
         Utility method used to format missing permission strings for a user-friendly response
 
@@ -280,6 +347,20 @@ class SettingsCommands(commands.Cog):
             missing_perms = missing_perms.replace(',', '')
         return missing_perms
 
+    @staticmethod
+    async def get_json_response(url, params, ctx):
+        request = requests.get(url, params=params)
+        if request.status_code == 404:
+            await ctx.send(f'Couldn\'t find {params["name"]} MMR on EUNE')
+            return None
+        request = request.json()
+        return request
+
 
 def setup(client):
+    client.add_cog(LolUtilityFunctions(client))
+    client.add_cog(TftUtilityFunctions(client))
+    client.add_cog(RiotUtilityFunctions(client))
+    client.add_cog(DatabaseManager(client))
+    client.add_cog(FilesManager(client))
     client.add_cog(SettingsCommands(client))
