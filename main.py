2f37ae5e16ca5c4bef78387c31c8c26edeb102db
donciop123@gmail.com <hzaUdz8hYqUg4Ar>
2025-01-04 03:32:27 +0100

    Added logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import nextcord  # main packages
 from nextcord.ext import commands
+from logger import get_logger
 import os  # utility packages
 
 # Making sure the bot has all the permissions
@@ -8,11 +9,15 @@ intents = nextcord.Intents().all()
 # Initialize bot
 client = commands.Bot(command_prefix='*', intents=intents, help_command=None)
 
+# Setting up logging
+bot_logger = get_logger()
+
 # Loading cogs
 for filename in os.listdir("COGS"):  # iterate over files in 'COGS' dictionary
-    print(filename)
     if filename.endswith(".py"):
         client.load_extension(f"COGS.{filename[:-3]}")  # load cogs into bot
-        print("Cog Loaded!")
+        bot_logger.info(f"Cog: {filename} loaded!")
 
+# Starting bot
+bot_logger.info("Bot is starting...")
 client.run(os.getenv('ALPHATOKEN'))  # actually run the bot and pass the secret TOKEN
